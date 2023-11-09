fdf9146640d6daefc9fb17effeac28f889badab3
SirLevid <jassontejada@gmail.com>
2023-11-09 07:12:10 -0400

    Arreglando campos para el post

--- app/src/main/java/com/levid/levid_p2_ap2/data/remote/dtos/GastoDto.kt
+++ app/src/main/java/com/levid/levid_p2_ap2/data/remote/dtos/GastoDto.kt
@@ -4,10 +4,12 @@ import com.squareup.moshi.Json
 
 data class GastoDto(
     @Json(name = "idGasto")
-    val id: Int,
+    val id: Int = 0,
     val fecha: String,
-    val suplidor: String?,
+    val idSuplidor: Int,
+    val suplidor: String? = null,
     val concepto: String,
+    val descuento: Int = 0,
     val ncf: String,
     val itbis: Int,
     val monto: Int
